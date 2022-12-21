7e597128974bea02b7cb43a2026e284b50bfbace
rambip <62420525+rambip@users.noreply.github.com>
2022-12-21 17:36:07 +0000

    Deploying to gh-pages from @ rambip/web-perfect-clear@da74b1608bb94bcae4c68219e7b76f52cb5590fa 🚀

--- perfect_clear-4b68ccef542b5b92.js
+++ perfect_clear-4b68ccef542b5b92.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('perfect_clear-7ef86bbe441a4043_bg.wasm', import.meta.url);
+        input = new URL('perfect_clear-4b68ccef542b5b92_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -486,7 +486,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper147 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper148 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 55, __wbg_adapter_18);
         return addHeapObject(ret);
     };
