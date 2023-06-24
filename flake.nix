2027140bcf6e66f3ed86c05976becebd75b633a8
rambip <apero1808@gmail.com>
2023-06-24 14:06:06 +0200

    updated wasm-tooling deps

--- flake.nix
+++ flake.nix
@@ -2,21 +2,18 @@
     description = "webapp to enumerate every perfect-clear";
     inputs.wasm-tooling.url = github:rambip/wasm-tooling;
 
-    outputs = {self, nixpkgs, wasm-tooling}: {
-        defaultPackage.x86_64-linux = 
-            let pkgs = import nixpkgs {system = "x86_64-linux";};
-                tooling = pkgs.callPackage wasm-tooling {};
-            in
-            tooling.rust.buildWithTrunk {
-                src = ./.;
-                fixRelativeUrl = true;
-            };
-        devShell.x86_64-linux = 
-            let pkgs = import nixpkgs {system = "x86_64-linux";};
-                tooling = pkgs.callPackage wasm-tooling {};
-            in
-            tooling.rust.devShell {
-                src = ./.;
-            } ;
+    outputs = {self, nixpkgs, wasm-tooling}: 
+        let forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ];
+        in
+    {
+
+        packages = forAllSystems (system:
+            {
+                default = wasm-tooling.lib."${system}".rust.buildWithTrunk {
+                    src = ./.;
+                    fixRelativeUrl = true;
+                };
+            });
+        devShells = wasm-tooling.devShells;
     };
 }
